33eef4adadfbd3fb0b7d33cecff9e29323694cb9
Tim Smith <tsmith@chef.io>
2016-05-19 14:09:16 -0700

    Update metadata
    redhat not rhel
    add long_description
    add source_url and issues_url
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer       'Joshua Timberman'
 maintainer_email 'cookbooks@housepub.org'
 license          'Apache 2.0'
 description      'Installs ucspi-tcp'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.2.0'
 
 recipe 'ucspi-tcp', 'Installs ucspi-tcp based on "install_method" attribute'
@@ -14,6 +15,9 @@ recipe 'ucspi-tcp::source', 'Installs ucspi-tcp via compiled source'
   depends cb
 end
 
-%w( ubuntu debian centos rhel arch ).each do |os|
+%w( ubuntu debian centos redhat arch ).each do |os|
   supports os
 end
+
+source_url 'https://github.com/jtimberman/ucspi-tcp' if respond_to?(:source_url)
+issues_url 'https://github.com/jtimberman/ucspi-tcp/issues' if respond_to?(:issues_url)
